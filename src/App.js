70c7180562ea0bef116117b2085dc59c01c6dad5
AJ <alexanderjessediaz@gmail.com>
2020-07-29 13:34:37 -0600

    import bootstrap, add cards, images, imputs, forms. remove react inital react stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col';
+import cup from './images/cup.jpg';
+import tablespoon from './images/tablespoon.jpg';
+import teaspoon from './images/teaspoon.jpg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid="md">
+        <CardDeck>
+          <Col xs="auto">
+            <Card>
+              <Card.Img variant="top" src={cup}/>
+              <Card.Body>
+              <Form.Group>
+                <Form.Row>
+                  <Form.Label column="lg" md={5}>
+                    Cup
+                  </Form.Label>
+                
+                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                  </Form.Row>
+                </Form.Group>
+                <Card.Title>
+                  <Card.Text>
+
+                  </Card.Text>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs="auto">
+            <Card>
+              <Card.Img variant="top" src={tablespoon}/>
+              <Card.Body>
+              <Form.Group>
+                <Form.Row>
+                  <Form.Label column="lg" md={5}>
+                    Tablespoon
+                  </Form.Label>
+                
+                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                  </Form.Row>
+                </Form.Group>
+                <Card.Title>
+                  <Card.Text>
+
+                  </Card.Text>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs="auto">
+            <Card>
+              <Card.Img variant="top" src={teaspoon}/>
+              <Card.Body>
+              <Form.Group>
+                <Form.Row>
+                  <Form.Label column="lg" md={5}>
+                    Teaspoon
+                  </Form.Label>
+                
+                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                  </Form.Row>
+                </Form.Group>
+                <Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </CardDeck>
+      </Container>
     </div>
   );
 }
