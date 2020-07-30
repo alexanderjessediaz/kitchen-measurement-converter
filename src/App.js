8309564cdf17664413279bb85893beeaf081f0e2
AJ <alexanderjessediaz@gmail.com>
2020-07-29 20:58:02 -0600

    add jumbotron, container, and media from bootstrap. add new spoon image. remove old images.

--- src/App.js
+++ src/App.js
@@ -2,81 +2,119 @@ import React from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container'
+import Media from 'react-bootstrap/Media';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import cup from './images/cup.jpg';
-import tablespoon from './images/tablespoon.jpg';
-import teaspoon from './images/teaspoon.jpg'
+import measuringspoons from './images/measuringspoons.jpg';
+
+
 
 
 function App() {
   return (
     <div className="App">
       <Container fluid="md">
-        <CardDeck>
-          <Col xs="auto">
-            <Card>
-              <Card.Img variant="top" src={cup}/>
-              <Card.Body>
-              <Form.Group>
-                <Form.Row>
-                  <Form.Label column="lg" md={5}>
-                    Cup
-                  </Form.Label>
-                
-                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                  </Form.Row>
-                </Form.Group>
-                <Card.Title>
-                  <Card.Text>
+          <Jumbotron fluid>
+            <Container>
+              <Media>
+                <img
+                  width={100}
+                  height={100}
+                  className="mr-3"
+                  src={measuringspoons}
+                  alt="measuring spoons"
+                />
+                <Media.Body>
+                  <h5>Kitchen Measurement Converter</h5>
+                  <p>
+                    Enter an amount for one of the measurements and see the converted measuremtns for all of the others.
+                  </p>
+                </Media.Body>
+              </Media>
+            </Container>
+          </Jumbotron>
+        <Row className="justify-content-md-center">
+          <CardDeck >
+            <Col xs="auto">
+              <Card>
+                <Card.Body>
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Label column="lg" md={5}>
+                     Cup
+                    </Form.Label>
+                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                    </Form.Row>
+                  </Form.Group>
+                  <Card.Title>
+                    <Card.Text>
 
-                  </Card.Text>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs="auto">
-            <Card>
-              <Card.Img variant="top" src={tablespoon}/>
-              <Card.Body>
-              <Form.Group>
-                <Form.Row>
-                  <Form.Label column="lg" md={5}>
-                    Tablespoon
-                  </Form.Label>
-                
-                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                  </Form.Row>
-                </Form.Group>
-                <Card.Title>
-                  <Card.Text>
-
-                  </Card.Text>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs="auto">
-            <Card>
-              <Card.Img variant="top" src={teaspoon}/>
-              <Card.Body>
-              <Form.Group>
-                <Form.Row>
-                  <Form.Label column="lg" md={5}>
-                    Teaspoon
-                  </Form.Label>
-                
-                    <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                  </Form.Row>
-                </Form.Group>
-                <Card.Title>
-                  <Card.Text>
-                  </Card.Text>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        </CardDeck>
+                    </Card.Text>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs="auto">
+              <Card>
+                <Card.Body>
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Label column="lg" md={5}>
+                      Tablespoon
+                    </Form.Label>
+                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                    </Form.Row>
+                  </Form.Group>
+                  <Card.Title>
+                    <Card.Text>
+                    </Card.Text>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs="auto">
+              <Card>
+                <Card.Body>
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Label column="lg" md={5}>
+                      Teaspoon
+                    </Form.Label>
+                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                    </Form.Row>
+                  </Form.Group>
+                  <Card.Title>
+                    <Card.Text>
+                    </Card.Text>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </CardDeck>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Cup</th>
+                <th>Tablespoon</th>
+                <th>Teaspoon</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+    </Table>
+        </Row>
       </Container>
     </div>
   );
