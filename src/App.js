74a6ebeb35321154c4c70afb28625404ef21ed6d
AJ <alexanderjessediaz@gmail.com>
2020-07-31 16:07:23 -0600

    rename CalculationTable. restructure CalcTable. add state to App.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
@@ -9,101 +9,121 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import measuringspoons from './images/measuringspoons.jpg';
-import calculationTable from "./components/calculationTable"
+import CalculationTable from "./components/CalculationTable";
 
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <Container fluid="md">
-          <Jumbotron fluid>
-            <Container>
-              <Media>
-                <img
-                  width={100}
-                  height={100}
-                  className="mr-3"
-                  src={measuringspoons}
-                  alt="measuring spoons"
-                />
-                <Media.Body>
-                  <h5>Kitchen Measurement Converter</h5>
-                  <p>
-                    Enter an amount for one of the measurements and see the converted measuremtns for all of the others.
-                  </p>
-                </Media.Body>
-              </Media>
-            </Container>
-          </Jumbotron>
-        <Row className="justify-content-md-center">
-          <CardDeck >
-            <Col xs="auto">
-              <Card>
-                <Card.Body>
-                <Form.Group>
-                  <Form.Row>
-                    <Form.Label column="lg" md={5}>
-                     Cup
-                    </Form.Label>
-                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                    </Form.Row>
-                  </Form.Group>
-                  <Card.Title>
-                    <Card.Text>
-                    </Card.Text>
-                  </Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col xs="auto">
-              <Card>
-                <Card.Body>
-                <Form.Group>
-                  <Form.Row>
-                    <Form.Label column="lg" md={5}>
-                      Tablespoon
-                    </Form.Label>
-                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                    </Form.Row>
-                  </Form.Group>
-                  <Card.Title>
-                    <Card.Text>
-                    </Card.Text>
-                  </Card.Title>
-                  
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col xs="auto">
-              <Card>
-                <Card.Body>
-                <Form.Group>
-                  <Form.Row>
-                    <Form.Label column="lg" md={5}>
-                      Teaspoon
-                    </Form.Label>
-                      <Form.Control size="lg" id="cup" placeholder="enter measurement" />
-                    </Form.Row>
-                  </Form.Group>
-                  <Card.Title>
-                    <Card.Text>
-                    </Card.Text>
-                  </Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-          </CardDeck>
-        </Row>
-        <Row className="justify-content-md-center">
-          <div>
-          {calculationTable}
-          </div>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    measurementTools: [
+      {
+        name: "Cup",
+        measurement: 0
+      },
+      {
+        name: "Tablespoon",
+        measurement: 0
+      },
+      {
+        name: "Teaspoon",
+        measurement: 0
+      },
+    ]
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <Container fluid="md">
+            <Jumbotron fluid>
+              <Container>
+                <Media>
+                  <img
+                    width={100}
+                    height={100}
+                    className="mr-3"
+                    src={measuringspoons}
+                    alt="measuring spoons"
+                  />
+                  <Media.Body>
+                    <h5>Kitchen Measurement Converter</h5>
+                    <p>
+                      Enter an amount for one of the measurements and see the converted measuremtns for all of the others.
+                    </p>
+                  </Media.Body>
+                </Media>
+              </Container>
+            </Jumbotron>
+          <Row className="justify-content-md-center">
+            <CardDeck >
+              <Col xs="auto">
+                <Card>
+                  <Card.Body>
+                  <Form.Group>
+                    <Form.Row>
+                      <Form.Label column="lg" md={5}>
+                       Cup
+                      </Form.Label>
+                        <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                      </Form.Row>
+                    </Form.Group>
+                    <Card.Title>
+                      <Card.Text>
+                      </Card.Text>
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col xs="auto">
+                <Card>
+                  <Card.Body>
+                  <Form.Group>
+                    <Form.Row>
+                      <Form.Label column="lg" md={5}>
+                        Tablespoon
+                      </Form.Label>
+                        <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                      </Form.Row>
+                    </Form.Group>
+                    <Card.Title>
+                      <Card.Text>
+                      </Card.Text>
+                    </Card.Title>
+                    
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col xs="auto">
+                <Card>
+                  <Card.Body>
+                  <Form.Group>
+                    <Form.Row>
+                      <Form.Label column="lg" md={5}>
+                        Teaspoon
+                      </Form.Label>
+                        <Form.Control size="lg" id="cup" placeholder="enter measurement" />
+                      </Form.Row>
+                    </Form.Group>
+                    <Card.Title>
+                      <Card.Text>
+                      </Card.Text>
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </CardDeck>
+          </Row>
+          
+            <CalculationTable/>
+          
+        </Container>
+      </div>
+
+    );
+  }
 }
 
 export default App;
--- src/components/calculationTable.js
+++ src/components/calculationTable.js
@@ -1,10 +1,30 @@
-import React from "react"
+import React from 'react'
+import Table from 'react-bootstrap/Table'
 
-function table() {
+function CalculationTable(props) {
     return (
-        <div>
-            <p>text</p>
-        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                <th>Utensil</th>
+                <th>Measurement</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Cup</td>
+                <td>0</td>
+                </tr>
+                <tr>
+                <td>Tablespoon</td>
+                <td>0</td>
+                </tr>
+                <tr>
+                <td>Teaspoon</td>
+                <td>0</td>
+                </tr>
+            </tbody>
+        </Table>
     )
 }
-export default table
+export default CalculationTable
